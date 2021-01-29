6900c5e9616fccb305fe8ed5f6f81924f7a81ff1
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:13:33 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 3 stazioni della regione VALLEAOSTA
+#'
+#' @format Un tibble con 8 colonne e 7778 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 2 stazioni della regione VALLEAOSTA
+#'
+#' @format Un tibble con 8 colonne e 4942 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
