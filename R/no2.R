2e7d31102bdb8b2ad74f0d52e404c88de20464d7
guidofioravanti <guidofioravanti@yahoo.it>
2021-03-19 15:27:39 +0100

    Introdotte serie complete solo per il 2020

--- R/no2.R
+++ R/no2.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di no2 per 3 stazioni della regione VALLEAOSTA
+#' Tibble con i dati di no2 per 4 stazioni della regione VALLEAOSTA
 #'
-#' @format Un tibble con 8 colonne e 7778 osservazioni
+#' @format Un tibble con 8 colonne e 8058 osservazioni
 #'
 #' @usage
 #' no2
---
+++ R/nox.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di nox per 4 stazioni della regione VALLEAOSTA
+#'
+#' @format Un tibble con 8 colonne e 2215 osservazioni
+#'
+#' @usage
+#' nox
+"nox"
